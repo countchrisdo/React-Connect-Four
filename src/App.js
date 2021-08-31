178d6f07476b2a22325f67ca716a5b69aebb5e8c
Christopher Burwell <crburwell@yahoo.com>
2021-08-31 12:21:58 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+import ConnectFour from "./ConnectFour";
+
+export default function App() {
+  return (
+    <div className="App">
+      <img src="https://i2.wp.com/www.baytekent.com/wp-content/uploads/2017/06/connect4logo.png?fit=433%2C129&ssl=1" />
+      <ConnectFour />
+    </div>
+  );
+}
---
+++ src/Pages/ConnectFourBoard.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import ReactDOM from "react-dom";
+
+// import Gameboard from "../Components/Gameboard";
+import "./ConnectFour.css";
+
+export default function Board() {
+  return <section className="ConnectFour"></section>;
+}
